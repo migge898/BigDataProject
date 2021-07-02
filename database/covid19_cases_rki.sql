0f77aba70987e86a4d85636759bdcf211820058f
Florian Saurwein <florian@saurwein.net>
2021-07-02 09:36:40 +0200

    Update table create statement

--- database/covid19_cases_rki.sql
+++ database/covid19_cases_rki.sql
@@ -13,6 +13,8 @@ create table covid19_cases_rki (
 	differenz_zum_vortag_raw varchar(20),
 	faelle_letzte_7_tage_raw varchar(20),
 	_7_tage_inzidenz_raw varchar(20),
-	todesfaelle_raw varchar(20)
+	todesfaelle_raw varchar(20),
+
+	html_raw longtext
 	
 );
